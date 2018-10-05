c6adb02b87c72de040644e69d9f021b3d9ef6086
Michael Hausenblas <michael.hausenblas@gmail.com>
2018-10-05 21:18:55 +0100

    updates e2e test

--- e2e.sh
+++ e2e.sh
@@ -5,6 +5,8 @@ set -o errtrace
 set -o nounset
 set -o pipefail
 
+# for i in {1..10} ; do kubectl -n krs get events -o json > out/${i}_e2e-events.json ; sleep 1; done
+
 echo "Launching two deployments"
 
 kubectl -n krs run appserver --image centos:7 -- \
@@ -13,9 +15,8 @@ kubectl -n krs run appserver --image centos:7 -- \
 kubectl -n krs run otherserver --image centos:7 -- \
            sh -c "while true; do echo WORK ; sleep 10 ; done"
 
+echo "Creating service"
 kubectl -n krs expose deploy/appserver --port 80
-
-echo "Waiting 2 sec"
 
 sleep 2
 
