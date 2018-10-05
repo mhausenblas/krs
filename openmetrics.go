f96c83c834af8a65bdd94ef7e036a30a894ffce4
Michael Hausenblas <michael.hausenblas@gmail.com>
2018-10-05 15:54:16 +0100

    fixes labels handling in OM line

--- openmetrics.go
+++ openmetrics.go
@@ -3,11 +3,13 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"strings"
 	"time"
 )
 
-// toOpenMetrics takes a JSON formatted kubectl result of a list of events
-// and turns it into a sequence of OpenMetrics lines.
+// toOpenMetrics takes the result of a `kubectl get events` as a
+// JSON formatted string as an input and turns it into a
+// sequence of OpenMetrics lines.
 func toOpenMetrics(rawevents string) string {
 	events := K8sEvents{}
 	err := json.Unmarshal([]byte(rawevents), &events)
@@ -17,7 +19,7 @@ func toOpenMetrics(rawevents string) string {
 	var oml string
 	for _, event := range events.Items {
 		if event.InvolvedObjectRef.Kind == "Pod" {
-			labels := map[string]string{"namespace": event.InvolvedObjectRef.Namespace}
+			labels := map[string]string{"name": event.InvolvedObjectRef.Name, "namespace": event.InvolvedObjectRef.Namespace}
 			oml += ometricsline("pod_count_all", "gauge", "Number of pods in any state (running, terminating, etc.)", "1", labels)
 		}
 	}
@@ -33,8 +35,12 @@ func ometricsline(metric, mtype, mdesc, value string, labels map[string]string) 
 	line += fmt.Sprintf("# TYPE %v %v\n", metric, mtype)
 	line += fmt.Sprintf("%v{", metric)
 	for k, v := range labels {
-		line += fmt.Sprintf("%v=\"%v\",", k, v)
+		line += fmt.Sprintf("%v=\"%v\"", k, v)
+		line += ","
 	}
+	// make sure that we get rid of trialing comma:
+	line = strings.TrimSuffix(line, ",")
+
 	line += fmt.Sprintf("} %v %v\n", value, time.Now().UnixNano())
 	return
 }
