b465b3ff607c5e1305eeacd4857f1e9b13950ee5
Michael Hausenblas <michael.hausenblas@gmail.com>
2018-10-05 21:12:38 +0100

    updates e2e test

--- e2e.sh
+++ e2e.sh
@@ -13,10 +13,13 @@ kubectl -n krs run appserver --image centos:7 -- \
 kubectl -n krs run otherserver --image centos:7 -- \
            sh -c "while true; do echo WORK ; sleep 10 ; done"
 
-echo "Waiting 30 sec now"
+kubectl -n krs expose deploy/appserver --port 80
 
-sleep 30
+echo "Waiting 2 sec"
+
+sleep 2
 
 echo "Deleting two deployments now"
 
-kubectl -n krs delete deploy/appserver deploy/otherserver
+kubectl -n krs delete deploy/appserver
+kubectl -n krs delete svc/appserver 
