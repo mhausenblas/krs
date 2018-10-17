f6e95795f54a075ce51a4a4787048aef654af4fd
Michael Hausenblas <michael.hausenblas@gmail.com>
2018-10-17 11:36:37 +0100

    improves e2e testing

--- e2e-test/run-e2e-test.sh
+++ e2e-test/run-e2e-test.sh
@@ -5,20 +5,36 @@ set -o errtrace
 set -o nounset
 set -o pipefail
 
+NAMESPACE="${1:-krs}"
+
+# make sure the namespace exists before we proceed:
+kubectl get ns | grep $NAMESPACE > /dev/null || (echo Aborting e2e test since I can not find namespace $NAMESPACE && exit 1)
+
+# deploy,rc,pods
 echo "Launching two deployments"
-
-kubectl -n krs run appserver --image centos:7 -- \
+kubectl -n $NAMESPACE run appserver --image centos:7 -- \
            sh -c "while true; do echo WORK ; sleep 10 ; done"
 
-kubectl -n krs run otherserver --image centos:7 -- \
+kubectl -n $NAMESPACE run otherserver --image centos:7 -- \
            sh -c "while true; do echo WORK ; sleep 10 ; done"
 
-echo "Creating service"
-kubectl -n krs expose deploy/appserver --port 80
+# svc
+echo "Creating a service"
+kubectl -n $NAMESPACE expose deploy/appserver --port 80
 
 sleep 10
 
-echo "Deleting two deployments now"
+echo "Deleting two deployments and the service"
+kubectl -n $NAMESPACE delete deploy/appserver deploy/otherserver
+kubectl -n $NAMESPACE delete svc/appserver 
 
-kubectl -n krs delete deploy/appserver deploy/otherserver
-kubectl -n krs delete svc/appserver 
+# ds
+
+# sts
+
+# jobs
+
+# cj
+
+# hpa
+
