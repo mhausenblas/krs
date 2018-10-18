0999018be0f2583815e5444d1f81b614534b9a51
Michael Hausenblas <michael.hausenblas@gmail.com>
2018-10-18 14:00:50 +0100

    improves e2e testing

--- e2e-test/run-e2e-test.sh
+++ e2e-test/run-e2e-test.sh
@@ -22,14 +22,18 @@ kubectl get ns | grep $NAMESPACE > /dev/null || (echo Aborting e2e test since I 
 # echo "Creating a daemon set"
 # kubectl -n $NAMESPACE apply -f ds.yaml
 # sleep 2
+# echo "Deleting the daemon set"
 # kubectl delete -n $NAMESPACE ds krs-test-ds
 
 ### sts
-echo "Creating a stateful set"
+echo "Creating a stateful set and service"
 kubectl -n $NAMESPACE apply -f sts.yaml
 sleep 10
+echo "Deleting the stateful set and service"
 kubectl delete -n $NAMESPACE svc krs-test-sts-svc
 kubectl delete -n $NAMESPACE sts krs-test-sts
+sleep 10
+echo "Deleting the persistent volume"
 kubectl delete -n $NAMESPACE pvc data-krs-test-sts-0
 
 ### jobs
